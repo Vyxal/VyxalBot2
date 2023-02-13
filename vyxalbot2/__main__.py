b20b2ba90866aff55fd34f9623515c26660a4b39
Ginger <75683114+gingerindustries@users.noreply.github.com>
2023-02-13 21:14:24 +0000

    Formatting and moving of things

--- vyxalbot2/__main__.py
+++ vyxalbot2/__main__.py
@@ -1,25 +1,3 @@
-import json
-import logging
-import sys
-import asyncio
+from vyxalbot2 import run
 
-from aiohttp.web import run_app
-
-from vyxalbot2 import VyxalBot2
-
-CONFIG_PATH = "config.json"
-
-logging.basicConfig(
-    format="[%(name)s] %(levelname)s: %(message)s",
-    stream=sys.stdout,
-    level=logging.DEBUG,
-)
-
-
-with open(CONFIG_PATH, "r") as f:
-    config = json.load(f)
-
-async def makeApp():
-    return VyxalBot2(config)
-
-run_app(makeApp(), port=config["port"])
+run()
