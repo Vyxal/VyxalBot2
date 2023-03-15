6acec6d40e50c39c3ac4719bdeb33f698c644a78
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-03-16 00:32:44 +1100

    Update replydb.py

--- vyxalbot2/replydb.py
+++ vyxalbot2/replydb.py
@@ -3,3 +3,15 @@ from typing import Optional
 from tinydb import TinyDB, Query
 from tinydb.table import Document
 
+class ReplyDB:
+  def __init__(self, dbPath: str):
+    self._db = TinyDB(dbPath)
+    
+  def getCorrespondingId(self, msg: int) -> Optional[Document]:
+    return r[0] if len(r := self._db.search(Query().parentId == msg)) else None
+
+  def addReplyToDatabase(self, messageMap: list):
+    self._db.insert({"parentId": messageMap[0], "botMessageId": messageMap[1]})
+  
+  def removeReplyId(self, msg: int):
+    self._db.remove(Query().parentId == user)
